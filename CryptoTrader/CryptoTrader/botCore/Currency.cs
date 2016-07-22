ed15aa7e67f2f926444baac2fde90c934987c460
Carrdinal <jamescarr1993@gmail.com>
2016-07-22 19:08:33 +0100

    Made changes to Currency class
    Started using XML documentation for methods using:
    https://msdn.microsoft.com/en-us/library/aa288481(v=vs.71).aspx as a
    reference.
    
    Changed the way Currency is initialised and made the symbol variable
    type string as some currencies use multiple character symbols.

--- CryptoTrader/CryptoTrader/botCore/Currency.cs
+++ CryptoTrader/CryptoTrader/botCore/Currency.cs
@@ -4,16 +4,32 @@ namespace CryptoTrader
 {
 	public class Currency
 	{
-		string type;
-
-		char symbol;
-
-		public Currency () 
+		private string type;
+        private double last;
+        private double buy;
+        private double sell;
+		private string symbol;
+        /*
+         * Initialise a currency with its type and symbol.
+         * eg. type:"GBP" symbol:"£"
+         * Symbol is string since there are symbols with 
+         * multiple characters, suchs as "kr" or "RUB"
+         */
+		public Currency (string type, string symbol) 
 		{
-			
+            this.type = type;
+            this.symbol = symbol;
 		}
 
-		public double convert ( double newCurrency )
+        /// <summary>
+        /// The convert method takes an amount and a currency. 
+        /// This amount is related to the instance of currency and converts the amount
+        /// into the other currency.
+        /// </summary>
+        /// <param name="currency"> This is the currency we want to convert to.</param>
+        /// <param name="amount"> This is the amount we want to convert.</param>
+        /// <returns> Returns the value of the amount of .this currency in the specified currency.</returns>
+		public double convert ( Currency currency, double amount )
 		{
 			double profit = 0;
 			//...
